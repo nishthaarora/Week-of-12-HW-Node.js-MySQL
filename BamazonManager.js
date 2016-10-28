cbc2d161165a4bf4ec6a0688c2c613287f5e6ad3
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-10-27 23:39:47 -0500

    adding itemID validation

--- BamazonManager.js
+++ BamazonManager.js
@@ -26,135 +26,159 @@ function displayData() {
 
 }
 
+function validateItemId() {
+	return new Promise(function(resolve, reject) {
+		var choice = [];
+		connection.query('SELECT * FROM products', function(err, res) {
+			if (err) throw err;
+			res.forEach(function(ele) {
+				choice.push(ele.ItemID);
+			})
+			resolve(choice);
+
+		})
+	});
+}
+
 
 function updateItemId() {
-
-	inquirer.prompt([{
-		name: 'oldId',
-		type: 'input',
-		message: 'Enter a old ItemID'
-	}, {
-		name: 'newId',
-		type: 'input',
-		message: 'Enter a new ItemID'
-	}]).then(function(answer) {
-		var query = 'UPDATE products SET ItemID = ? WHERE ItemID = ?';
-		connection.query(query, [answer.newId, answer.oldId], function(err, res) {
-			if (err) throw err;
-			console.log('New Id has been updated');
-			console.log('----------------------------------------------------------');
-			// new Promise(function(resolve, reject) {
+	validateItemId().then(function(choice) {
+		inquirer.prompt([{
+			name: 'oldId',
+			type: 'list',
+			message: 'Enter a old ItemID',
+			choices: choice
+		}, {
+			name: 'newId',
+			type: 'input',
+			message: 'Enter a new ItemID'
+		}]).then(function(answer) {
+			var query = 'UPDATE products SET ItemID = ? WHERE ItemID = ?';
+			connection.query(query, [answer.newId, answer.oldId], function(err, res) {
+				if (err) throw err;
+				console.log('New Id has been updated');
+				console.log('----------------------------------------------------------');
 				displayData();
-				// resolve();
-			// }).then(function() {
-			// 	restart();
-			// })
-		})
-	})
+			});
+		});
+	});
 }
 
 
 function updateProductName() {
-	inquirer.prompt([{
-		name: 'ItemID',
-		type: 'input',
-		message: 'Enter an Item Id which you wish to update',
 
-	}, {
-		name: 'productName',
-		type: 'input',
-		message: 'Enter a new Product Name'
-	}]).then(function(answer) {
-		var product = answer.productName.toUpperCase();
-		var query = 'UPDATE products SET productName = ? WHERE ItemID = ?';
-		connection.query(query, [product, answer.ItemID], function(err, res) {
-			if (err) throw err;
-			console.log('New Product Name has been updated');
-			console.log('----------------------------------------------------------');
-			displayData()
+	validateItemId().then(function(choice) {
+
+		inquirer.prompt([{
+			name: 'ItemID',
+			type: 'list',
+			message: 'Enter an Item Id which you wish to update',
+			choices: choice
+		}, {
+			name: 'productName',
+			type: 'input',
+			message: 'Enter a new Product Name'
+		}]).then(function(answer) {
+			var product = answer.productName.toUpperCase();
+			var query = 'UPDATE products SET productName = ? WHERE ItemID = ?';
+			connection.query(query, [product, answer.ItemID], function(err, res) {
+				if (err) throw err;
+				console.log('New Product Name has been updated');
+				console.log('----------------------------------------------------------');
+				displayData()
+			})
 		})
 	})
 }
 
 function updateDepartmentName() {
-	var choice=[];
-	connection.query('SELECT * FROM departments', function (err, res) {
-		if(err)throw err;
-		res.forEach(function(ele){
-			choice.push(ele.DepartmentName);
+	var deptChoice = [];
+	connection.query('SELECT * FROM departments', function(err, res) {
+		if (err) throw err;
+		res.forEach(function(ele) {
+			deptChoice.push(ele.DepartmentName);
 		})
 	})
 
-	console.log('Note: If the department you are looking for doesnot exist, kindly contact your supervisor to get the department added');
+	validateItemId().then(function(choice) {
+		console.log('Note: If the department you are looking for doesnot exist, kindly contact your supervisor to get the department added');
+		inquirer.prompt([{
+			name: 'ItemID',
+			type: 'list',
+			message: 'Enter an Item Id which you wish to update',
+			choices: choice
+		}, {
+			name: 'DepartmentName',
+			type: 'list',
+			message: 'which department would you like to add this product in',
+			choices: deptChoice
+		}]).then(function(answer) {
 
-	inquirer.prompt([{
-		name: 'ItemID',
-		type: 'input',
-		message: 'Enter an Item Id which you wish to update'
-	}, {
-		name: 'DepartmentName',
-		type: 'list',
-		message: 'which department would you like to add this product in',
-		choices: choice
-	}]).then(function(answer) {
-
-				var query = 'UPDATE products SET DepartmentName = ? WHERE ItemID = ?';
-				connection.query(query, [answer.DepartmentName, answer.ItemID], function(err, res) {
-					if (err) throw err;
-					console.log('New Department Name has been updated');
-					console.log('----------------------------------------------------------');
-					displayData()
-				})
+			var query = 'UPDATE products SET DepartmentName = ? WHERE ItemID = ?';
+			connection.query(query, [answer.DepartmentName, answer.ItemID], function(err, res) {
+				if (err) throw err;
+				console.log('New Department Name has been updated');
+				console.log('----------------------------------------------------------');
+				displayData()
+			})
 		})
+	})
 }
 
 function updatePrice() {
-	inquirer.prompt([{
-		name: 'ItemID',
-		type: 'input',
-		message: 'Enter an Item Id which you wish to update'
-	}, {
-		name: 'Price',
-		type: 'input',
-		message: 'Enter a new Price',
-		validate: function(answer) {
-			if (answer > 0) {
-				return true;
-			}
-		}
-	}]).then(function(answer) {
 
-		var query = 'UPDATE products SET Price = ? WHERE ItemID = ?';
-		connection.query(query, [answer.Price, answer.ItemID], function(err, res) {
-			if (err) throw err;
-			console.log('New Price has been updated');
-			console.log('----------------------------------------------------------');
-			displayData()
+	validateItemId().then(function(choice) {
+		inquirer.prompt([{
+			name: 'ItemID',
+			type: 'list',
+			message: 'Enter an Item Id which you wish to update',
+			choices: choice
+		}, {
+			name: 'Price',
+			type: 'input',
+			message: 'Enter a new Price',
+			validate: function(answer) {
+				if (answer > 0) {
+					return true;
+				}
+			}
+		}]).then(function(answer) {
+
+			var query = 'UPDATE products SET Price = ? WHERE ItemID = ?';
+			connection.query(query, [answer.Price, answer.ItemID], function(err, res) {
+				if (err) throw err;
+				console.log('New Price has been updated');
+				console.log('----------------------------------------------------------');
+				displayData()
+			})
 		})
 	})
 }
 
 function updateStockQuantity() {
-	inquirer.prompt([{
-		name: 'ItemID',
-		type: 'input',
-		message: 'Enter an Item Id which you wish to update'
-	}, {
-		name: 'StockQuantity',
-		type: 'input',
-		message: 'Enter a new StockQuantity',
-		validate: function(answer) {
-			if (answer > 0) {
-				return true;
+	validateItemId().then(function(choice) {
+		inquirer.prompt([{
+			name: 'ItemID',
+			type: 'list',
+			message: 'Enter an Item Id which you wish to update',
+			choices: choice
+		}, {
+			name: 'StockQuantity',
+			type: 'input',
+			message: 'Enter a new StockQuantity',
+			validate: function(answer) {
+				if (answer > 0) {
+					return true;
+				}
 			}
-		}
-	}]).then(function(answer) {
-		var query = 'UPDATE products SET StockQuantity = ? WHERE ItemID = ?';
-		connection.query(query, [answer.StockQuantity, answer.ItemID], function(err, res) {
-			if (err) throw err;
-			console.log('New StockQuantity has been updated');
-			console.log('----------------------------------------------------------');
-			displayData()
+		}]).then(function(answer) {
+			var query = 'UPDATE products SET StockQuantity = ? WHERE ItemID = ?';
+			connection.query(query, [answer.StockQuantity, answer.ItemID], function(err, res) {
+				if (err) throw err;
+				console.log('New StockQuantity has been updated');
+				console.log('----------------------------------------------------------');
+				displayData()
+			})
 		})
 	})
 }
